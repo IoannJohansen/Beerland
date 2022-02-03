95690e7fd4d43e3a14f4562ec22d1c0e95a3828c
Ioann Sachishin <boss.vanya20@mail.ru>
2022-02-03 22:21:52 +0300

    Created axiosHandler class ( contains wrappers of axios for http requests ) for api calls.

--- BeerlandWeb/Scripts/statistic.ts
+++ BeerlandWeb/Scripts/statistic.ts
@@ -3,8 +3,9 @@ import DatePicker from "@/DatePicker.vue";
 import StatisticDrawer from "@/StatisticDrawer.vue";
 import ISeriesEntry from "../Utils/Interfaces/ISeriesEntry";
 import IDate from "../Utils/Interfaces/IDate";
-import { getStatisticByDate } from "../Utils/Api/StatisticApi";
 import IStatisticViewModel from "../Utils/Interfaces/IStatisticViewModel";
+import AxiosHandler from "../Utils/Api/AxiosHandler";
+import {GET_STAT_BY_DATE} from "../Utils/Api/ApiBase";
 
 export default class StatisticPageApp extends BasePage{
     
@@ -35,7 +36,7 @@ export default class StatisticPageApp extends BasePage{
         Month: new Date().getMonth()+1,
         Day: new Date().getDate(),
     }
-
+    
     private series : Array<ISeriesEntry> = [{
         name: 'Target',
         data: []
@@ -47,27 +48,28 @@ export default class StatisticPageApp extends BasePage{
     private categories : Array<String> = []
     
     private onDatePickHandler(date : IDate) {
-        getStatisticByDate<Array<IStatisticViewModel>>(date, (data)=>{
-                this.series = [{
-                    name: 'Target',
-                    data: data.data.map((item)=>item.target)
-                }, {
-                    name: 'Produced',
-                    data: data.data.map((item)=>item.produced)
-                }];
-                this.categories = data.data.map((item)=>item.beerMark)
-            },
-            (data) => {
-                this.series = [{
-                    name: 'Target',
-                    data: []
-                }, {
-                    name: 'Produced',
-                    data: []
-                }];
-                this.categories = []
-                console.log("Error status: " + data.status)
-            });
+        const requestParam = {
+            date: `${date.Year}.${date.Month}.${date.Day}`
+        }
+        AxiosHandler.axiosGet<Object, Array<IStatisticViewModel>>(requestParam, GET_STAT_BY_DATE, (data : Array<IStatisticViewModel>)=>{
+            this.series = [{
+                name: 'Target',
+                data: data.map((item)=>item.target)
+            }, {
+                name: 'Produced',
+                data: data.map((item)=>item.produced)
+            }];
+            this.categories = data.map((item)=>item.beerMark)
+        },(reqConfig) => {
+            this.series = [{
+                name: 'Target',
+                data: []
+            }, {
+                name: 'Produced',
+                data: []
+            }];
+            this.categories = []
+        })
     }
     
     private mounted(){
---
+++ BeerlandWeb/Utils/Api/AxiosHandler.ts
@@ -0,0 +1,33 @@
+﻿import axios, {AxiosResponse} from "axios";
+
+export default  class AxiosHandler {
+    static axiosGet = async <I, O>(data : I, url : string, onSuccess : Function, onFail? : Function) => {
+        const response = await axios.get<AxiosResponse<O>>(url, {
+            params:{
+                ...data,
+            }
+        });
+        if (response.status === 200){
+            onSuccess(response.data);
+        }else{
+            console.log(`Error: ${response.status}`);
+            if (onFail) {
+                onFail(response);
+            }
+        }
+    }
+
+    static axiosPost = async <I, O>(data : I, url : string, onSuccess : Function, onFail? : Function) => {
+        const response = await axios.post<AxiosResponse<O>>(url, {
+            ...data,
+        });
+        if (response.status === 200){
+            onSuccess(response.data);
+        }else{
+            console.log(`Error: ${response.status}`);
+            if (onFail) {
+                onFail(response);
+            }
+        }
+    }
+}
